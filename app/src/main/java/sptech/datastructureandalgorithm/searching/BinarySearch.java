d3cec0b33d6b2758ce69091b5a13c1a74033bc81
Prasad <sp.dobest@gmail.com>
2018-06-06 07:22:15 +0530

    Sorting images and programs added

--- app/src/main/java/sptech/datastructureandalgorithm/searching/BinarySearch.java
+++ app/src/main/java/sptech/datastructureandalgorithm/searching/BinarySearch.java
@@ -37,4 +37,23 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    public int binarySearch(int[] inputArr, int key) {
+
+        int start = 0;
+        int end = inputArr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (key == inputArr[mid]) {
+                return mid;
+            }
+            if (key < inputArr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
 }
